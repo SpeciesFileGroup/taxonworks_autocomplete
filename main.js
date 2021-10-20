00ae7d81f7f1e20afa41fe76dce040ee1cca2351
José Luis Pereira <pepelui@outlook.com>
2021-10-20 12:15:21 -0300

    Add cdn to example

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ codeElement.innerText = `
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sfgrp/tw-autocomplete@0.0.1/dist/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@sfgrp/tw-autocomplete@0.0.1/dist/tw-autocomplete.umd.min.js"></script>
     <title>TaxonWorks Autocomplete</title>
   </head>
   <body>
