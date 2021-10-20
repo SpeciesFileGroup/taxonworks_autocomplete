cf23327e3cf586112f958ca3443b24bcbf58aef9
José Luis Pereira <pepelui@outlook.com>
2021-10-20 12:11:36 -0300

    Add example

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 import './src/assets/stylesheets/tw-autocomplete.css'
 import './src/assets/stylesheets/style.css'
-import { TWAutocomplete } from './src/autocomplete.js'
+import TWAutocomplete from './src/autocomplete.js'
 
-const autocomplete = new TWAutocomplete('#taxonworks-autocomplete', {
-  resource: 'taxon_names',
-  url: 'http://localhost:3000',
-  api_version: 1,
-  project_id: 16,
-  events: {
-    select: item => { console.log(item) }
-  }
-})
+const codeElement = document.querySelector('pre')
+codeElement.innerText = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TaxonWorks Autocomplete</title>
+  </head>
+  <body>
+    <form 
+      action="https://sfg.taxonworks.org/api/v1/taxon_names.json" 
+      method="get"
+    >
+      <div>
+        <input
+          hidden
+          name="project_token"
+          value="xQ9bKrhfQtHYfro9t6YY0A"
+        >
+        <input 
+          data-taxonworks-autocomplete="true"
+          data-resource="taxon_names"
+          data-name="taxon_name_id[]"
+          data-project-token="xQ9bKrhfQtHYfro9t6YY0A"
+          data-url="https://sfg.taxonworks.org/"
+          placeholder="Search..."
+        >
+      </div>
+      <button type="submit">
+        JSON
+      </button>
+    </form>
+  </body>
+</html>`
